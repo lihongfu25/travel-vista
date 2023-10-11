477251d7ea33f70e3b6dad72c1fea9374e19f128
phulh-dev <phuhd2k1@gmail.com>
2023-10-11 17:38:55 +0700

    feat: image upload

--- libs/frontend/components/src/lib/image-upload-control/image-upload-control.tsx
+++ libs/frontend/components/src/lib/image-upload-control/image-upload-control.tsx
@@ -1,12 +1,17 @@
-import { Input } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import FormLabel from '@mui/material/FormLabel';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 import { styled } from '@mui/system';
 import React from 'react';
 import { Controller } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
 import { ImageUploadControlProps } from '../types';
 import styles from './image-upload-control.module.scss';
+
 const StyledFormControl = styled(FormControl)`
   .MuiFormLabel-asterisk {
     color: red;
@@ -20,23 +25,31 @@ const StyledFormLabel = styled(FormLabel)`
 /* eslint-disable-next-line */
 
 export function ImageUploadControl(props: ImageUploadControlProps) {
-  const [selectedImage, setSelectedImage] = React.useState<File | null>(null);
-  const [uploadProgress, setUploadProgress] = React.useState<number | null>(
+  const { t } = useTranslation();
+  const [selectedImages, setSelectedImages] = React.useState<File[] | null>(
     null
   );
 
-  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (file) {
-      setSelectedImage(file);
+  const handleImageChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const fileList = event.target.files;
+    if (fileList) {
+      const files = Array.from(fileList);
+      setSelectedImages(files);
+      return files.map((file) => URL.createObjectURL(file));
     }
+    return null;
   };
 
-  const handleUpload = async () => {
-    if (selectedImage) {
-      const formData = new FormData();
-      formData.append('image', selectedImage);
-    }
+  const handleRemoveImage = (image: File) => {
+    const newFiles = selectedImages
+      ? selectedImages?.filter(
+          (file) => file.lastModified !== image.lastModified
+        )
+      : null;
+    setSelectedImages(newFiles);
+    return newFiles?.map((file) => URL.createObjectURL(file)) || null;
   };
 
   return (
@@ -56,16 +69,63 @@ export function ImageUploadControl(props: ImageUploadControlProps) {
             <StyledFormLabel className="mb-1">{props.label}</StyledFormLabel>
           )}
           <div className={styles['image-upload']}>
-            {selectedImage && (
-              <img
-                src={URL.createObjectURL(selectedImage)}
-                alt="Preview"
-                width="100"
-                className={styles['image-upload__preview']}
+            {selectedImages?.map((image, i) => (
+              <div className={styles['image-upload__preview']}>
+                <img
+                  key={i}
+                  src={URL.createObjectURL(image)}
+                  alt="Preview"
+                  className="object-fit-cover w-100 h-100"
+                />
+                <div
+                  className={`${styles['image-upload__preview__backdrop']} position-absolute top-0 bottom-0 start-0 end-0 rounded-4`}
+                >
+                  <Tooltip title={t('common.delete')}>
+                    <IconButton
+                      aria-label="delete"
+                      onClick={() => {
+                        const urls = handleRemoveImage(image);
+                        field.onChange(urls);
+                      }}
+                    >
+                      <DeleteOutlinedIcon
+                        sx={{
+                          color: '#f5f5f5',
+                          fontSize: '1.5rem',
+                        }}
+                      />
+                    </IconButton>
+                  </Tooltip>
+                </div>
+              </div>
+            ))}
+            <div
+              className={`${styles['image-upload__input']} ${
+                Boolean(props.errors) && styles['error']
+              }`}
+            >
+              <AddIcon
+                sx={{
+                  fontSize: '1.5rem',
+                  color: '#1a1a1a',
+                }}
               />
-            )}
-            <div className={styles['image-upload__input']}>
-              <input type="file" {...field} onChange={handleImageChange} />
+              <p
+                className="mb-0 mt-2"
+                style={{
+                  color: '#1a1a1a',
+                }}
+              >
+                {t('common.upload')}
+              </p>
+              <input
+                type="file"
+                multiple={props.multiple}
+                onChange={async (e) => {
+                  const urls = await handleImageChange(e);
+                  field.onChange(urls);
+                }}
+              />
             </div>
           </div>
           <FormHelperText>{props.errors?.message}</FormHelperText>
