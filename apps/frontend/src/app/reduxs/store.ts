8f9623a7647f1688ffd2d20af9255b44b22ae6bc
phulh-dev <phuhd2k1@gmail.com>
2024-11-04 21:34:13 +0700

    refactor: cache auth state

--- apps/frontend/src/app/reduxs/store.ts
+++ apps/frontend/src/app/reduxs/store.ts
@@ -1,12 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/user';
 import layoutReducer from './admin-layout/admin-layout';
+import storageSession from 'redux-persist/lib/storage/session';
+import { persistStore, persistReducer } from 'redux-persist';
+import { environment } from '@frontend/configuration';
+
+const persistConfig = {
+  key: environment.authPersistKey,
+  storage: storageSession,
+};
+
+const userPersistedReducer = persistReducer(persistConfig, userReducer);
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: userPersistedReducer,
     layout: layoutReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
 });
+
+const persistor = persistStore(store);
+
+export { persistor };
 
 export default store;
--- apps/frontend/src/app/reduxs/user/user.ts
+++ apps/frontend/src/app/reduxs/user/user.ts
@@ -1,4 +1,7 @@
+import { environment } from '@frontend/configuration';
+import { Role } from '@frontend/model';
 import { createSlice } from '@reduxjs/toolkit';
+import { isEmpty, omit } from 'lodash-es';
 
 const userSlice = createSlice({
   name: 'user',
@@ -10,6 +13,24 @@ const userSlice = createSlice({
     clearUser: () => null,
   },
 });
+
+const manageRoles = new Set(environment.manage);
+
+/* eslint-disable-next-line */
+export const getUserState = ({ user }: { user: any }) => omit(user, '_persist');
+
+/* eslint-disable-next-line */
+export const isUserAuthenticated = ({ user }: { user: any }) => {
+  return !isEmpty(omit(user, '_persist'));
+};
+
+export const isAdmin = (state: { user: { roles: Role[] } | null }) => {
+  const userRoles = state.user?.roles;
+  if (userRoles) {
+    return userRoles.some(({ slug }) => manageRoles.has(slug));
+  }
+  return false;
+};
 
 export const { setUser, clearUser } = userSlice.actions;
 export default userSlice.reducer;
